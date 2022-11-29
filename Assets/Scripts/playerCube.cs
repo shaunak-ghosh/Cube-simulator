867c9a3dc843fd122cf78dd685b9cb75987eaef9
Shaunak Ghosh <98326430+Rooveloftcodes@users.noreply.github.com>
2022-11-29 23:51:48 +0530

    changed some things on level 1

--- Assets/Scripts/playerCube.cs
+++ Assets/Scripts/playerCube.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading;
 using UnityEngine;
 
 public class playerCube : MonoBehaviour
@@ -8,6 +10,7 @@ public class playerCube : MonoBehaviour
     bool jumpKeyWasPressed;
     private Rigidbody rigidBodyComponent;
     int jumpsRemaining;
+    float defaultSpeed = 2.4f;
 
     void Start()
     {
@@ -27,11 +30,11 @@ public class playerCube : MonoBehaviour
     {
         if(autoMove == true)
         {
-            rigidBodyComponent.velocity = new Vector3(horizontalInput * 2.4f, rigidBodyComponent.velocity.y, 7f);
+            rigidBodyComponent.velocity = new Vector3(horizontalInput * defaultSpeed, rigidBodyComponent.velocity.y, 7f);
         }
         else 
         { 
-            rigidBodyComponent.velocity = new Vector3(horizontalInput * 2.4f, rigidBodyComponent.velocity.y, verticalInput*7);
+            rigidBodyComponent.velocity = new Vector3(horizontalInput * defaultSpeed, rigidBodyComponent.velocity.y, verticalInput*7);
         }
         if(jumpKeyWasPressed == true)
         {
@@ -42,13 +45,23 @@ public class playerCube : MonoBehaviour
     private void jump()
     {
         GetComponent<Rigidbody>().AddForce(Vector3.up * 6f, ForceMode.VelocityChange);
+        Debug.Log("Jumps remaining: " + jumpsRemaining);
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.layer == 6)
         {
-            Destroy(other.gameObject);
-            jumpsRemaining++;
+            Destroy(other.gameObject);//not needed!
+            jumpsRemaining++; 
         }
+        if(other.gameObject.layer == 7)
+        {
+            defaultSpeed = 3.0f;
+            speedUpTimer();
+        }
+    }
+    private void speedUpTimer()
+    {
+        throw new NotImplementedException();
     }
 }
