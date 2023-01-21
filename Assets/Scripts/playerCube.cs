2504c1f815874e1c4109dc2058a8a2baa1434603
Shaunak Ghosh <98326430+Rooveloftcodes@users.noreply.github.com>
2023-01-21 20:07:01 +0530

    Fixed jump bug, modified speed-boost texture

--- Assets/Scripts/playerCube.cs
+++ Assets/Scripts/playerCube.cs
@@ -1,61 +1,62 @@
 using UnityEngine;
-
 public class playerCube : MonoBehaviour
 {
     public bool autoMove;
-    private float verticalInput, horizontalInput, timeRemaining = 10;
-    bool jumpKeyPressed, boost = false;
+    private float verticalInput, horizontalInput, timeRemaining = 6;
+    bool jumpable,jumpKeyPressed = false, boost = false, move = true;
     private Rigidbody rigidBodyComponent;
     int jumpsRemaining;
-    float defaultSpeedZ = 7f, defaultSpeedX = 2.4f; //z is forward and x is sideways
-
+    float defaultSpeedZ = 7f, defaultSpeedX = 2.4f;
     void Start()
     {
         rigidBodyComponent = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        //bug not fixed yet. Check collision with ground
-        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) && jumpsRemaining > 0 && GetComponent<Transform>().position.y <= 0.9973934)
+        if (move == true)
         {
-            jumpKeyPressed = true;
-            jumpsRemaining -= 1;
-            jump();
-            Debug.Log("Jumps remaining" + jumpsRemaining);
+            if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) && jumpsRemaining >=1 && jumpable == true)
+            {
+                jumpKeyPressed = true;
+                jumpsRemaining -= 1;
+                jump();
+            }
+            verticalInput = Input.GetAxis("Vertical");
+            horizontalInput = Input.GetAxis("Horizontal");
         }
-        verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal");
         if (boost == true)
         {
             if (timeRemaining > 1)
             {
-                Debug.Log("Timer is running" + timeRemaining);
                 timeRemaining -= Time.deltaTime;
             }
             else if (timeRemaining < 1)
             {
-                Debug.Log("Timer has run out");
                 boost = false;
-                timeRemaining = 10;
+                timeRemaining = 6;
                 defaultSpeedZ= 7f;
             }
         }
     }
     private void FixedUpdate()
     {
-        if (autoMove == true)
+        if (move == true)
         {
-            rigidBodyComponent.velocity = new Vector3(horizontalInput * defaultSpeedX, rigidBodyComponent.velocity.y, defaultSpeedZ);
-        }
-        else
-        {
-            rigidBodyComponent.velocity = new Vector3(horizontalInput * defaultSpeedX, rigidBodyComponent.velocity.y, verticalInput * defaultSpeedZ);
+            if (autoMove == true)
+            {
+                rigidBodyComponent.velocity = new Vector3(horizontalInput * defaultSpeedX, rigidBodyComponent.velocity.y, defaultSpeedZ);
+            }
+            else
+            {
+                rigidBodyComponent.velocity = new Vector3(horizontalInput * defaultSpeedX, rigidBodyComponent.velocity.y, verticalInput * defaultSpeedZ);
+            }
         }
     }
     private void jump()
     {
         GetComponent<Rigidbody>().AddForce(Vector3.up * 6f, ForceMode.VelocityChange);
         Debug.Log("Jumps remaining: " + jumpsRemaining);
+        jumpable = false;
         jumpKeyPressed = false;
     }
     private void OnTriggerEnter(Collider other)
@@ -70,10 +71,13 @@ public class playerCube : MonoBehaviour
             Destroy(other.gameObject);
             speedBoost();
         }
-        if (other.gameObject.layer == 8 && GetComponent<Transform>().position.y == 291.78)
+        if(other.gameObject.layer == 8)
         {
-            defaultSpeedX = 0;
-            defaultSpeedZ = 0;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+        }
+        if(other.gameObject.layer == 9)
+        {
+            jumpable = true;
         }
     }
     private void speedBoost()
