b309e4de0a3c23200d37ffa1b241df44ac932e82
Shaunak Ghosh <98326430+Rooveloftcodes@users.noreply.github.com>
2023-02-20 22:18:07 +0530

    Added automatic moving obstacle

---
+++ Assets/Scripts/orangeBlock.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class orangeBlock : MonoBehaviour
+{
+    [SerializeField] float distanceToCover;
+    [SerializeField] float speed;
+
+    private Vector3 startingPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+    void Update()
+    {
+        Vector3 v = startingPosition;
+        v.x += distanceToCover * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
