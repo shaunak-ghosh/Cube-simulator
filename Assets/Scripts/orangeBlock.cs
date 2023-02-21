9a1b1a3e392752f0a22cc308660ba1b46157d0bf
Shaunak Ghosh <98326430+Rooveloftcodes@users.noreply.github.com>
2023-02-21 22:01:31 +0530

    Added orange moving barrier blocks

--- Assets/Scripts/orangeBlock.cs
+++ Assets/Scripts/orangeBlock.cs
@@ -6,7 +6,6 @@ public class orangeBlock : MonoBehaviour
     [SerializeField] float speed;
 
     private Vector3 startingPosition;
-
     // Start is called before the first frame update
     void Start()
     {
